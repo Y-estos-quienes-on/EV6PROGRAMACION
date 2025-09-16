42c555f149552012563d423645d2df9fc5a2e265
fernandallanos13 <llanosfernanda1@gmail.com>
2025-09-15 21:37:02 -0300

    Actualizo test usuario con pytest
    Lo ejecuté y esta perfecccccto

--- TEST/test_usuario.py
+++ TEST/test_usuario.py
@@ -1,25 +1,38 @@
-import unittest
+import pytest
 from POO_SMARTHOME.usuario import Usuario
 
-class TestUsuario(unittest.TestCase):
+@pytest.fixture
+def user():
+    return Usuario("Paola", "pao1@gmail.com", 1234, "general")
 
-    def setUp(self):
-                self.user = Usuario("Paola", "pao1@gmail.com", "1234", "general")
-    
-    def test_registrar(self):
-        resultado = self.user.registrar()
-        self.assertEqual(resultado, "Usuario Paola está registrado como general")
-    def test_iniciar_sesion_correcta(self):
-        self.assertTrue(self.user.iniciar_sesion("Paola", "1234"))
+def test_registrar(user):
+    resultado = user.registrar()
+    assert resultado == "Usuario Paola está registrado como general"
 
-    def test_iniciar_sesion_incorrecta(self):
-        self.assertFalse(self.user.iniciar_sesion("Paola", "0000"))
+def test_iniciar_sesion_correcta(user):
+    assert user.iniciar_sesion("Paola" , "1234") is True
 
-    def test_consultar_datos(self):
-        datos = self.user.consultar_datos()
-        self.assertEqual(datos["usuario"], "Paola")
-        self.assertEqual(datos["email"], "pao1@gmail.com")
-        self.assertEqual(datos["rol"], "general")
+def test_consultar_datos(user):
+    datos = user.consultar_datos()
+    assert datos["usuario"] == "Paola"
+    assert datos["email"] == "pao1@gmail.com"
+    assert datos["rol"] == "general"
 
-if __name__ == "__main__":
-    unittest.main()
+def test_cambiar_contraseña(user):
+    user.cambiar_contraseña("abcd")
+    assert user.iniciar_sesion("Paola", "abcd") is True
+
+def test_cambiar_contraseña_insegura(user):
+    with pytest.raises(ValueError) as excinfo:
+        user.cambiar_contraseña("123")  
+    assert "La contraseña es insegura" in str(excinfo.value)
+
+def test_email_invalido():
+    with pytest.raises(ValueError) as excinfo:
+        Usuario("Ana", "email_sin_arroba", "1234", "general")
+    assert "Email inválido" in str(excinfo.value)
+
+def test_rol_invalido():
+    with pytest.raises(ValueError) as excinfo:
+        Usuario("Ana", "ana@mail.com", "1234", "superuser")
+    assert "Rol inválido" in str(excinfo.value)
