5d962446bc490ed12d53ee6ec8a11c69f0e088d5
fernandallanos13 <llanosfernanda1@gmail.com>
2025-09-16 08:28:29 -0300

    Update usuario- cambiados los self por el bucle infinito.py

--- POO_SMARTHOME/usuario.py
+++ POO_SMARTHOME/usuario.py
@@ -1,8 +1,8 @@
-class Usuario: 
+class Usuario:
     ROLES_VALIDOS = ["general", "admin", "invitado"]
-    
+
     def __init__(self, usuario, email, contraseña, rol):
-        self.usuario = usuario
+        self._usuario = usuario
         self._email = email
         self.__contraseña = None
         self.cambiar_contraseña(contraseña)
@@ -10,41 +10,49 @@ class Usuario:
 
     @property
     def usuario(self):
-        return self.usuario
+        return self._usuario
 
-    @usuario.setter 
-    def usuario(self, valor): 
-        if not valor or valor.strip() == "":
+    @usuario.setter
+    def usuario(self, valor):
+        if not valor.strip():
             raise ValueError("El nombre de usuario no puede estar vacío")
-        self.usuario = valor
+        self._usuario = valor
 
     @property
     def email(self):
-        return self.email
-    
-    @email.setter 
-    def email(self,valor):
-        if "@" not in valor or "."not in valor:
+        return self._email
+
+    @email.setter
+    def email(self, valor):
+        if "@" not in valor or "." not in valor:
             raise ValueError("Email inválido")
-        self.email = valor
+        self._email = valor
 
     @property
     def rol(self):
-        return self.rol
-    
+        return self._rol
+
     def registrar(self):
-        return f"Usuario {self.usuario} está registrado como {self.rol}"
-    
+        return f"Usuario {self._usuario} está registrado como {self._rol}"
+
     def iniciar_sesion(self, usuario, contraseña):
-        return self.usuario == usuario and self.__contraseña == contraseña
-    
+        return self._usuario == usuario and self.__contraseña == contraseña
+
     def consultar_datos(self):
-        return {
-            "usuario": self.usuario,
-            "email": self.email,
-            "rol": self.rol 
-        }
+        return {"usuario": self._usuario, "email": self._email, "rol": self._rol}
+
     def cambiar_contraseña(self, nueva_contraseña):
-        if len(nueva_contraseña) <4:
+        if len(nueva_contraseña) < 4:
             raise ValueError("La contraseña es demasiado corta")
         self.__contraseña = nueva_contraseña
+
+
+class Administrador(Usuario):
+    def modificar_rol(self, usuario, nuevo_rol):
+        if nuevo_rol not in Usuario.ROLES_VALIDOS:
+            raise ValueError("Rol inválido")
+        usuario._rol = nuevo_rol
+
+
+class General(Usuario):
+    pass
