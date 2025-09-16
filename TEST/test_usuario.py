a6fac76f5c55aedaac1ca13a3d9327fe4356f732
fernandallanos13 <llanosfernanda1@gmail.com>
2025-09-16 09:12:14 -0300

    agrego dos pruebas a test_usuario.py

--- TEST/test_usuario.py
+++ TEST/test_usuario.py
@@ -12,6 +12,12 @@ def test_registrar(user):
 def test_iniciar_sesion_correcta(user):
     assert user.iniciar_sesion("Paola", "1234") is True
 
+def test_iniciar_sesion_incorrecta(user):
+    # Usuario incorrecto
+    assert user.iniciar_sesion("Otra", "1234") is False
+    # Contraseña incorrecta
+    assert user.iniciar_sesion("Paola", "wrongpass") is False
+
 def test_consultar_datos(user):
     datos = user.consultar_datos()
     assert datos["usuario"] == "Paola"
@@ -31,3 +37,8 @@ def test_email_invalido():
     with pytest.raises(ValueError) as excinfo:
         Usuario("Paola", "email_sin_arroba", "1234", "general")
     assert "Email inválido" in str(excinfo.value)
+
+def test_usuario_vacio():
+    with pytest.raises(ValueError) as excinfo:
+        Usuario("", "pao2@gmail.com", "1234", "general")
+    assert "El nombre de usuario no puede estar vacío" in str(excinfo.value)
