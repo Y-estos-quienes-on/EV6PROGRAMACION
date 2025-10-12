66eed0478fab28cae295246cd933afc140381bd2
MatePC <romano.matiasleonel@gmail.com>
2025-10-12 18:04:30 -0300

    Finalizado Login y Registro, se acomoda estructuracion de carpetas y archivos vacios

--- POO_SMARTHOME/dominio/usuarios/general.py
+++
@@ -1,5 +0,0 @@
-from dominio.usuarios.base import Usuario
-
-class General(Usuario):
-    def __init__(self, usuario, email, contraseña):
-        super().__init__(usuario, email, contraseña, rol="general")
